1d45ece066fdffaad1bfa55e2cc8be5cb2bce84a
Alexey <valeskey1988@mail.ru>
2023-02-15 21:05:02 +0500

    formate code

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,11 +1,10 @@
 public class CreditPaymentService {
-    public int calculate(int month, double percent, int summa)
-    {
-        double persMonht = (percent/12)/100;
+    public int calculate(int month, double percent, int summa) {
+        double persMonht = (percent / 12) / 100;
         double onePersMonth = 1 + persMonht;
-        double numerator = (summa*persMonht)*Math.pow(onePersMonth, month);
-        double denominator = Math.pow(onePersMonth, month)-1;
-        double pay = numerator/denominator;
+        double numerator = (summa * persMonht) * Math.pow(onePersMonth, month);
+        double denominator = Math.pow(onePersMonth, month) - 1;
+        double pay = numerator / denominator;
         return (int) pay;
     }
 
